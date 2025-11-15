87ff7438b3ac658a1b384665b1897ac6002aa2b3
𝐒𝐮𝐣𝐚𝐧 𝐃𝐚𝐬 <sd765715@gmail.com>
2025-11-16 01:02:14 +0530

    update

--- app/cars/[id]/page.tsx
+++ app/cars/[id]/page.tsx
@@ -61,14 +61,14 @@ export default function CarPage() {
       {/* NAVBAR */}
       <nav className="fixed top-0 left-0 right-0 z-20 backdrop-blur-xl bg-black/40 border-b border-white/10">
         <div className="max-w-7xl mx-auto px-4 sm:px-6 py-4 flex items-center justify-between">
-          <Link href="/" className="flex items-center gap-2 group">
+          <Link href="/" className="flex items-center  group">
   <img
     src="/logo.png"
     alt="RaceUp Logo"
     className="w-16 h-16 object-contain"
   />
 
-  <span className="font-script text-white text-2xl font-bold drop-shadow-lg group-hover:text-gray-300 transition">
+  <span className="font-script text-green-700 text-2xl font-bold drop-shadow-lg group-hover:text-gray-300 transition">
     RaceUp
   </span>
 </Link>
